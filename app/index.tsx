bed92f15333043e967f5517f1b059d8939fd8500
achramdani <achramdani18@gmail.com>
2024-11-12 23:25:14 +0700

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,41 @@
-import { Text, View, StyleSheet } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Text, View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 
 export default function Index() {
+  const [dataVehicles, setDataVehicles] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://11.11.15.241/api/vehicles');
+      const data = await response.json();
+      setDataVehicles(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>DASHBOARD</Text>
-    </View>
+    <ScrollView>
+      <View>
+        {dataVehicles ? (
+          dataVehicles.map((vehicles, key) => (
+            <View key={key}>
+              <Text>{vehicles.plat_nomor}</Text>
+              <Text>{vehicles.plat_merek}</Text>
+              <Text>{vehicles.model}</Text>
+              <Text>{vehicles.tahun}</Text>
+              <Text>{vehicles.keterangan}</Text>
+            </View>
+          ))
+        ) : (
+          <ActivityIndicator size="large" color="#007bff" />
+        )}
+      </View>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  }, 
-  header : {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  }
-});
